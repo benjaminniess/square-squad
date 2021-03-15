d015be5065d8cc5e52df4e2ada2648af43db0a96
Benjamin Niess <benjamin.niess@gmail.com>
2021-03-15 22:20:11 +0100

    Spawed players on connect

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
+let players = []
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/html/index.html')
@@ -20,10 +21,25 @@ http.listen(3000, () => {
   console.log('listening on *:3000')
 })
 
+io.on('connection', (socket) =>{
+  players.push({
+    'id': socket.id,
+    'x': 100 * (players.length+ 1),
+    'y': 200 * (players.length + 1)
+  })
+
+  socket.on('disconnect', function () {
+    players = players.filter(function(player) {
+      return player.id != socket.id 
+    })
+  });
+})
+
+
 setInterval(refreshData, 10)
 
 function refreshData() {
   io.emit('refreshCanvas', {
-    test: 'ok2',
+    'players' : players
   })
 }
