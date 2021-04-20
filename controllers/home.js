36c63febea807a99e5f7778937e6d60db7b91be3
Benjamin <benjamin.niess@gmail.com>
2021-04-20 13:22:56 +0200

    Pre-selected random color to users

--- controllers/home.js
+++ controllers/home.js
@@ -20,7 +20,10 @@ module.exports = function (app) {
     ) {
       res.render('index', {
         nickName: currentPlayer.nickName,
-        playerColor: currentPlayer.color,
+        playerColor: currentPlayer.color
+          ? currentPlayer.color
+          : '#' +
+            (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6),
         appVersion: appVersion,
       })
     } else {
