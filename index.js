022cb9ca825d0c0ea20af02c7d06d2f7e83bc13b
Benjamin <benjamin.niess@gmail.com>
2021-04-29 21:40:32 +0200

    Added new relic management

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+require('dotenv').config()
+
+// New relic agent option
+if (
+  process.env.ENABLE_NEW_RELIC_AGENT &&
+  process.env.ENABLE_NEW_RELIC_AGENT === 'true'
+) {
+  require('newrelic')
+}
+
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -5,7 +15,6 @@ const session = require('express-session')
 const cookieParser = require('cookie-parser')
 const { InMemorySessionStore } = require('./lib/sessionStore')
 const packageJson = require('./package.json')
-require('dotenv').config()
 
 app.set('trust proxy', 1) // trust first proxy
 app.use(
