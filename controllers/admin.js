dcd123bbe75c99f2cffa38d8b14e2e114f0b9054
benjaminniess <benjamin.niess@gmail.com>
2021-05-24 13:25:30 +0200

    Added admin page with memory usage stats

---
+++ controllers/admin.js
@@ -0,0 +1,52 @@
+'use_strict'
+
+const express = require('express')
+const router = express.Router()
+const Helpers = require('../lib/helpers')
+
+module.exports = function (app) {
+  app.use('/admin', router)
+
+  router.get('*', function (req, res, next) {
+    if (
+      !req.query.pwd ||
+      !process.env.ADMIN_PASSWORD ||
+      req.query.pwd != process.env.ADMIN_PASSWORD
+    ) {
+      res.send('Nop')
+    }
+
+    next()
+  })
+
+  /**
+   * The home URL
+   */
+  router.get('/', function (req, res, next) {
+    let memoryUsage = process.memoryUsage()
+
+    res.render('admin/index', {
+      playersCount: _.size(Helpers.getPlayers()),
+      roomsCount: _.size(Helpers.getRooms()),
+      memoryRSS: Math.round(memoryUsage.rss / 1024 / 1024) + 'Mb',
+      memoryheapTotal: Math.round(memoryUsage.heapTotal / 1024 / 1024) + 'Mb',
+      memoryheapUsed: Math.round(memoryUsage.heapUsed / 1024 / 1024) + 'Mb',
+      memoryexternal: Math.round(memoryUsage.external / 1024 / 1024) + 'Mb',
+      memoryarrayBuffers:
+        Math.round(memoryUsage.arrayBuffers / 1024 / 1024) + 'Mb',
+    })
+  })
+
+  router.get('/snapshot', function (req, res, next) {
+    const fs = require('fs')
+    const v8 = require('v8')
+    const snapshotStream = v8.getHeapSnapshot()
+    const localPath = `${process.cwd()}/public/`
+    const fileName = `${Date.now()}.headsnapshot`
+
+    const fileStream = fs.createWriteStream(localPath + fileName)
+    snapshotStream.pipe(fileStream)
+
+    res.redirect('/' + fileName)
+  })
+}
