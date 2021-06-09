6165ba62d7063fea3ac5718d538c60342d53af5c
Benjamin <benjamin.niess@gmail.com>
2021-06-09 08:16:07 +0200

    Added vuex

--- vueapp/src/main.js
+++ vueapp/src/main.js
@@ -2,12 +2,53 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    status: 'waiting',
+    roomName: null,
+    roomSlug: null,
+    players: {},
+    isAdmin: false,
+    currentPlayer: null,
+  },
+  mutations: {
+    roomJoined(state, roomData) {
+      state.players = roomData.players
+      state.currentPlayer = roomData.currentPlayer
+      state.isAdmin = roomData.isAdmin
+      state.status = roomData.status
+      state.roomName = roomData.roomName
+      state.roomSlug = roomData.roomSlug
+    },
+    refreshPlayers(state, refreshedPlayers) {
+      state.players = refreshedPlayers
+      refreshedPlayers.map((player) => {
+        if (player.isAdmin && player.id === state.currentPlayer) {
+          state.isAdmin = true
+        }
+      })
+    },
+    setGameStatus(state, gameStatus) {
+      if (gameStatus === 'playing') {
+        particles.style.display = 'none'
+      } else {
+        particles.style.display = 'block'
+      }
+      state.status = gameStatus
+    },
+  },
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store: store,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
 })
