4d08bde8ca6bc3a0c065dd4fd883a3271e8d439b
Benjamin <benjamin.niess@gmail.com>
2021-06-27 16:54:47 +0200

    Saved player name + color to local sotrage

--- vueapp/src/components/Home.vue
+++ vueapp/src/components/Home.vue
@@ -5,10 +5,22 @@
       <h1 class="sqsq-logo" title="Square Squad">
         <Logo />
       </h1>
-      <form class="sq-form" id="pre-home-form" method="post" action="/">
+      <form
+        class="sq-form"
+        id="pre-home-form"
+        method="post"
+        action="/"
+        @submit="checkForm"
+      >
         <div class="input-field">
           <label for="playerName">What's your name?</label
-          ><input id="playerName" type="text" name="playerName" required />
+          ><input
+            id="playerName"
+            type="text"
+            name="playerName"
+            required
+            v-model="playerName"
+          />
         </div>
         <div class="input-field">
           <label for="playerColor">What's your favourite color?</label
@@ -18,6 +30,7 @@
             name="playerColor"
             required
             value="#742a55"
+            v-model="playerColor"
           />
         </div>
         <div class="input-field input-submit text-center">
@@ -39,12 +52,24 @@
 <script>
 import Logo from './common/Logo'
 export default {
+  data() {
+    return {
+      playerName: localStorage.playerName ? localStorage.playerName : null,
+      playerColor: localStorage.playerColor
+        ? localStorage.playerColor
+        : '#' + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6)
+    }
+  },
   name: 'Home',
   components: {
     Logo
   },
   methods: {
-    submit() {}
+    checkForm(e) {
+      e.preventDefault()
+      localStorage.playerName = this.playerName
+      localStorage.playerColor = this.playerColor
+    }
   },
   computed: {
     version() {
