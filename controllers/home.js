765e2d41403de5ceb49a2cc919ca48e728b498fd
Benjamin <benjamin.niess@gmail.com>
2021-07-04 21:10:51 +0200

    Removed old pug templates

--- controllers/home.js
+++ controllers/home.js
@@ -13,29 +13,4 @@ module.exports = function (app) {
   router.get('/', function (req, res, next) {
     res.sendFile(__base + '/vueapp/dist/index.html')
   })
-
-  /**
-   * Save the new nickname in session. If the session is new, also create a session ID
-   */
-  router.post('/', function (req, res, next) {
-    if (!req.body.playerName) {
-      res.render('error', { message: 'Nickame is required' })
-    } else {
-      let playerObj = helpers.getPlayer(req.cookies['connect.sid'])
-
-      playerObj.resetData({
-        nickName: validator.blacklist(req.body.playerName, "<>\\/'"),
-        color: validator.blacklist(req.body.playerColor, "<>\\/'")
-      })
-
-      res.redirect('/rooms')
-    }
-  })
-
-  /**
-   * The about page
-   */
-  router.get('/about-us', function (req, res, next) {
-    res.render('about', {})
-  })
 }
