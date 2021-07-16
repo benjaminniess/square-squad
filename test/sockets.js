1af127a7f787085ad1191186db239cfe925c6f31
Benjamin Niess <bniess@sevanova.com>
2021-07-16 14:00:43 +0200

    Used a promise system to manage socket io tests

--- test/sockets.js
+++ test/sockets.js
@@ -1,10 +1,18 @@
 const { io } = require('socket.io-client')
-const socket = io('/', { transports: ['websocket'] })
+const socket = io('http://localhost:8080')
 const { expect } = require('chai')
 
 describe('SOCKET', function () {
   it('IO connection', function () {
-    socket.emit('update-player-data', { name: 'Tester', color: '#FF0000' })
-    return true
+    return new Promise((resolve, reject) => {
+      socket.emit('update-player-data', { name: 'Tester', color: '#FF0000' })
+
+      socket.on('player-data-updated', (data) => {
+        resolve(data)
+      })
+    }).then(result => {
+      expect(result.success).to.be.ok()
+    })
+
   })
 })
