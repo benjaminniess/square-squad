4d0d13e48fec5c282d989089df0b67733f157df3
Benjamin <benjamin.niess@gmail.com>
2021-07-23 15:20:43 +0200

    Added extra tests to LobbySection for non admin players

--- vueapp/test/unit/specs/LobbySection.spec.js
+++ vueapp/test/unit/specs/LobbySection.spec.js
@@ -52,11 +52,49 @@ describe('LobbySection.vue', () => {
     expect(wrapper.find('ul.players-list li').text()).toContain('[Admin]')
   })
 
-  it('should show that current player is flagged as crrent player', () => {
+  it('should show that current player is flagged as current player', () => {
     expect(wrapper.find('ul.players-list li').text()).toContain('[You]')
   })
 
-  it('should show that admin form to the admin player', () => {
+  it('should show admin form to the admin player', () => {
     expect(wrapper.findComponent({ name: 'AdminForm' }).exists()).toBe(true)
+  })
+
+  const wrapper2 = shallowMount(LobbySection, {
+    propsData: {
+      players: [
+        {
+          color: '#FF0000',
+          id: 'n7BGXs8MCOWDd3hvAAAR',
+          isAdmin: false, // Not admin anymore
+          nickname: 'Tester Admin',
+          score: 0
+        },
+        {
+          color: '#00FF00',
+          id: '"a6fJks8MCOWDd5ucBBBV"',
+          isAdmin: false, // is now admin
+          nickname: 'Tester client',
+          score: 0
+        }
+      ],
+      room: {
+        roomSlug: 'room-slug',
+        roomName: 'The Room name',
+        gameStatus: 'waiting'
+      }
+    }
+  })
+
+  it('should show that current player is not admin anymore', () => {
+    expect(wrapper2.find('ul.players-list li').text()).not.toContain('[Admin]')
+  })
+
+  it('should show still that current player is flagged as current player', () => {
+    expect(wrapper2.find('ul.players-list li').text()).toContain('[You]')
+  })
+
+  it('should not show admin form to the admin player', () => {
+    expect(wrapper2.findComponent({ name: 'AdminForm' }).exists()).toBe(false)
   })
 })
