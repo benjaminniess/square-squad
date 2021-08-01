40f23630c112edf19d481b3514a27ba2089a33b1
Benjamin <benjamin.niess@gmail.com>
2021-08-01 21:40:21 +0200

    Tested Room status changes

--- vueapp/test/unit/specs/Room.spec.js
+++ vueapp/test/unit/specs/Room.spec.js
@@ -1,11 +1,13 @@
 import Room from '@/components/Room'
-import { mount, config } from '@vue/test-utils'
+import { mount, config, createLocalVue } from '@vue/test-utils'
 
 let stepsToCall = {
   'room-join-socket': false
 }
 
 let mockRoomSlug = 'the-room-slug'
+
+const localVue = createLocalVue()
 
 config.mocks = {
   $globalEnv: {
@@ -41,7 +43,7 @@ config.mocks = {
 }
 
 describe('Room.vue', () => {
-  const wrapper = mount(Room)
+  const wrapper = mount(Room, { localVue })
 
   it('should emit a socket when mounted', () => {
     expect(stepsToCall['room-join-socket']).toBe(true)
@@ -62,6 +64,48 @@ describe('Room.vue', () => {
   it('should hide the RankSection component', () => {
     expect(wrapper.findComponent({ name: 'RankSection' }).isVisible()).toBe(
       false
+    )
+  })
+
+  it('should hide LobbySection after status update', async () => {
+    config.mocks.$store.state.gameStatus = 'playing'
+
+    await localVue.nextTick()
+    expect(wrapper.findComponent({ name: 'LobbySection' }).isVisible()).toBe(
+      false
+    )
+  })
+
+  it('should show the GameSection component', () => {
+    expect(wrapper.findComponent({ name: 'GameSection' }).isVisible()).toBe(
+      true
+    )
+  })
+
+  it('should hide the RankSection component', () => {
+    expect(wrapper.findComponent({ name: 'RankSection' }).isVisible()).toBe(
+      false
+    )
+  })
+
+  it('should hide GameSection after status update', async () => {
+    config.mocks.$store.state.gameStatus = 'end-round'
+
+    await localVue.nextTick()
+    expect(wrapper.findComponent({ name: 'GameSection' }).isVisible()).toBe(
+      false
+    )
+  })
+
+  it('should hide the LobbySection component', () => {
+    expect(wrapper.findComponent({ name: 'LobbySection' }).isVisible()).toBe(
+      false
+    )
+  })
+
+  it('should show the RankSection component', () => {
+    expect(wrapper.findComponent({ name: 'RankSection' }).isVisible()).toBe(
+      true
     )
   })
 })
