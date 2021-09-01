676c9aa6123c03182bc1195a9a77f809748dd740
Benjamin <benjamin.niess@gmail.com>
2021-09-01 16:58:41 +0200

    :wrench: Configured ServeStatic module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { AppService } from './app.service';
 import { PlayersModule } from './players/players.module';
 import { RoomsPlayersAssociationService } from './rooms/rooms-players-association.service';
 import { RoomsModule } from './rooms/rooms.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [PlayersModule, RoomsModule],
+  imports: [
+    PlayersModule,
+    RoomsModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '/../../vueapp/dist'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, RoomsPlayersAssociationService],
 })
