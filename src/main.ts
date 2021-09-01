51f144a8caadadadc38eb28c093980e2a6952e28
Benjamin <benjamin.niess@gmail.com>
2021-09-01 16:20:35 +0200

    :lock: Opened cors policy to localhost:1080

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  let corsOption = {};
+  if (process.env.NODE_ENV && process.env.NODE_ENV === 'development') {
+    corsOption = {
+      cors: {
+        origin: 'http://localhost:1080',
+        methods: ['GET', 'POST'],
+      },
+    };
+  }
+  app.enableCors(corsOption);
+
   await app.listen(8080);
 }
 bootstrap();
