c5d7d7111559ef45451e014dc8de77cc2537d073
Benjamin <benjamin.niess@gmail.com>
2021-11-26 10:08:57 +0100

    :wrench: Fixed image path for bonus

--- vueapp/src/components/GameSection.vue
+++ vueapp/src/components/GameSection.vue
@@ -75,7 +75,7 @@ export default {
     let ctx = canvas ? canvas.getContext('2d') : null
 
     let bonusImage = new Image()
-    bonusImage.src = this.$globalEnv.homeUrl + '/assets/images/bonus.png'
+    bonusImage.src = this.$globalEnv.homeUrl + '/static/assets/images/bonus.png'
 
     let currentTime = Date.now()
     let blinkOn = true
