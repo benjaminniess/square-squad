b4602695c9565b111aa65c14f56a1b0557f89189
Benjamin Niess <benjamin@web-id.fr>
2022-07-14 21:53:37 +0200

    :sparkles: Add logo component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import Logo from "./components/Logo";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+        <Logo/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
